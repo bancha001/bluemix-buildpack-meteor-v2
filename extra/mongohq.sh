802261389399a78e3b0c5996ae39f6bd2a8d8196
Bancha Setthanan <dukebkk@gmail.com>
2015-02-01 14:19:17 +1100

    remove root path

--- extra/mongohq.sh
+++ extra/mongohq.sh
@@ -1,10 +1,4 @@
 #!/usr/bin/env bash
 
 echo "MongoDB Service Binding" | indent
-mkdir -p "$BUILD_DIR"/.profile.d
-cat | tee "$BUILD_DIR"/.profile.d/mongo.sh <<EOF
-  #!/bin/sh
-  export ROOT_URL=\${ROOT_URL:-`ruby -e 'require "json"; puts "http://#{JSON.parse(ENV["VCAP_APPLICATION"])["application_uris"][0]}"'`}
-  export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| value[0]['label'].eql? 'mongolab' }[1][0]['credentials']['uri']"`}
-  export
-  EOF
+echo "export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| value[0]['label'].eql? 'mongolab' }[1][0]['credentials']['uri']"`}" > $BUILD_DIR"/.profile.d/mongo.sh"
