7bb221c6aa3d671c8fb1693f324761cd8ef455fb
Bancha Setthanan <dukebkk@gmail.com>
2015-09-04 23:09:00 +1000

    Update mongohq.sh
    Update Service extraction

--- extra/mongohq.sh
+++ extra/mongohq.sh
@@ -1,4 +1,9 @@
 #!/usr/bin/env bash
 
-echo "MongoHQ" | indent
-echo "export MONGO_URL=\${MONGO_URL:-\$MONGOHQ_URL}" > $BUILD_DIR"/.profile.d/mongo.sh"
+echo "MongoDB Service Binding" | indent
+mkdir -p "$BUILD_DIR"/.profile.d
+cat | tee "$BUILD_DIR"/.profile.d/mongo.sh <<EOF
+  #!/bin/sh
+  export ROOT_URL=\${ROOT_URL:-`ruby -e 'require "json"; puts "http://#{JSON.parse(ENV["VCAP_APPLICATION"])["application_uris"][0]}"'`}
+  export MONGO_URL=\${MONGO_URL:-`ruby -e "require 'json'; puts JSON.parse(ENV['VCAP_SERVICES']).find { |key,value| value[0]['label'].eql? 'mongolab' }[1][0]['credentials']['uri']"`}
+EOF
